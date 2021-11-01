4b31185797a0f038f468476f12ee21c49467d9e5
kpg <>
2021-11-01 12:48:45 +0000

    move examples into code
    FossilOrigin-Name: 0a4ffbb6500b7242a329ac8e754add4d2b48d84cfa667f71bd7e60ea4ceb1789

---
+++ examples/basic.lisp
@@ -0,0 +1,25 @@
+(require :asdf)
+(pushnew (truename "../") asdf:*central-registry*)
+(asdf:load-system :tuple)
+
+(setf tup (tuple:tuple 1 2 "foo" 3.14))
+
+(tuple:conj tup "bar")
+
+(tuple:pop tup)
+
+(tuple:insert tup 0 "baz")
+
+(tuple:lookup tup 3)
+
+(tuple:peek tup)
+
+(tuple:equal tup tup)
+
+(tuple:slice tup 1 3)
+
+(tuple:count tup)
+
+;; Optional `[` reader macro
+(asdf:load-system :tuple/reader)
+(tuple:equal [1 2 (+ 1 2)] (tuple:tuple 1 2 3))
