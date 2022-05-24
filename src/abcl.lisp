50149463a503bf73309a3281eb946bd2567d079b
kpg <>
2022-05-24 21:28:32 +0000

    progress on making java calls faster in abcl implementation
    FossilOrigin-Name: 87197aab8e933150ec6abaa772f675dd80d6fe2c93ec07ee88a32378c106b98c

--- src/abcl.lisp
+++ src/abcl.lisp
@@ -1,34 +1,41 @@
 (in-package :tuple)
 
-(defmethod tuple (&rest elems)
+(declare (optimize speed))
+(declaim (inline lookup insert conj count pop slice peek tuple))
+
+(defun tuple (&rest elems)
   (if (zerop (length elems))
       (java:jfield  "clojure.lang.PersistentVector" "EMPTY")
       (java:jstatic "adopt" "clojure.lang.PersistentVector"
                     (java:jarray-from-list elems))))
 
-(defvar persistent-vector (java:jclass "clojure.lang.PersistentVector"))
-(defvar get (java:jmethod persistent-vector "get" "int"))
+(defvar |Object| (java:jclass "java.lang.Object"))
+(defvar |PersistentVector| (java:jclass "clojure.lang.PersistentVector"))
+(defvar |get| (java:jmethod |PersistentVector| "get" "int"))
+(defvar |assoc| (java:jmethod |PersistentVector| "assoc" |Object| |Object|))
+(defvar |cons| (java:jmethod |PersistentVector| "cons" "java.lang.Object"))
+(defvar |count| (java:jmethod |PersistentVector| "count"))
+(defvar |pop| (java:jmethod |PersistentVector| "pop"))
+(defvar |subList| (java:jmethod |PersistentVector| "subList" "int" "int"))
+(defvar |peek| (java:jmethod |PersistentVector| "peek"))
 
-(defun %get (tuple index)
-  (java:jcall get tuple index))
+(defun lookup (tuple index)
+  (java:jcall |get| tuple index))
 
-(defmethod lookup (tuple index)
-  (%get tuple index))
+(defun insert (tuple index val)
+  (java:jcall |assoc| tuple index val))
 
-(defmethod insert (tuple index val)
-  (java:jcall "assoc" tuple index val))
+(defun conj (tuple val)
+  (java:jcall |cons| tuple val))
 
-(defmethod conj (tuple val)
-  (java:jcall "cons" tuple val))
+(defun count (tuple)
+  (java:jcall |count| tuple))
 
-(defmethod count (tuple)
-  (java:jcall "count" tuple))
+(defun pop (tuple)
+  (java:jcall |pop| tuple))
 
-(defmethod pop (tuple)
-  (java:jcall "pop" tuple))
+(defun slice (tuple start &optional end)
+  (java:jcall |subList| tuple start (or end (count tuple))))
 
-(defmethod slice (tuple start &optional end)
-  (java:jcall "subList" tuple start (or end (count tuple))))
-
-(defmethod peek (tuple)
-  (java:jcall "peek" tuple))
+(defun peek (tuple)
+  (java:jcall |peek| tuple))
